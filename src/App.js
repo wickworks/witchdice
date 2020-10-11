61cc793ec5c7a3fc65ae02145be86340bd0e8a67
Olive Perry <wick@nitidbit.com>
2020-10-11 15:13:26 -0700

    add beta test label

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.scss';
 import Main from './components/Main.jsx';
+import { CURRENT_VERSION } from './data.js';
 
 function App() {
   return (
     <div className="App">
       <h1>🌺💀 ~ Roll To Hit ~ 💀🌺</h1>
+      <div className='beta-label'>
+        beta test — v{CURRENT_VERSION}
+      </div>
 
       <Main />
 
