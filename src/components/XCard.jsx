9df3e2c5f7384c13aee45e00a56bd55a7760a1ea
Olive Perry <wick@nitidbit.com>
2021-01-09 09:19:20 -0800

    xcard flashes the tab title

--- src/components/XCard.jsx
+++ src/components/XCard.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
+import { Helmet } from "react-helmet";
 import FocusTrap from 'focus-trap-react'
 import './XCard.scss';
 
 let restoreFocusOnElement = null;
-
 
 const XCard = ({
   handleXCardRaise,
@@ -37,15 +37,31 @@ const XCard = ({
 	);
 }
 
+function setFlashTimeout(toggle, setToggle, storeTimeout, interval) {
+  storeTimeout(
+    setTimeout(() => {
+      setToggle(!toggle)
+      setFlashTimeout(!toggle, setToggle, storeTimeout, interval)
+    }, interval)
+  )
+}
+
 const XCardModal = ({
   raisedBy,
   handleClose,
 }) => {
-
+  const [overrideTitle, setOverrideTitle] = useState(true);
+  const [currentFlashTimeout, setCurrentFlashTimeout] = useState(null);
 
   useEffect(() => {
-    document.body.style.overflow = 'hidden'
+    setFlashTimeout(
+      overrideTitle,
+      setOverrideTitle,
+      setCurrentFlashTimeout,
+      2000
+    )
 
+    document.body.style.overflow = 'hidden'
     return () => {
       document.body.style.overflow = 'unset'
       if (restoreFocusOnElement) {
@@ -54,6 +70,11 @@ const XCardModal = ({
       }
     }
   }, []);
+
+  const closeModal = () => {
+    clearTimeout(currentFlashTimeout)
+    handleClose()
+  }
 
 	return ReactDOM.createPortal(
     <FocusTrap>
@@ -64,12 +85,16 @@ const XCardModal = ({
         role='alertdialogue'
         tabIndex='-1'
         onClick={handleClose}
-        onKeyDown={e => { if (e.keyCode === 27) handleClose() }}
+        onKeyDown={e => { if (e.keyCode === 27) closeModal() }}
       >
-        <div className='popup' onClick={e => e.stopPropagation()}>
-          <div className='whodunnit' id='whodunnit'>X card raised by {raisedBy}</div>
+        <Helmet>
+            {overrideTitle && <title>X-card raised</title>}
+        </Helmet>
 
-          <button aria-label="Close" onClick={handleClose} autoFocus>
+        <div className='popup' onClick={e => e.stopPropagation()}>
+          <div className='whodunnit' id='whodunnit'>X-card raised by {raisedBy}.</div>
+
+          <button aria-label="Close" onClick={closeModal} autoFocus>
             OK
           </button>
         </div>
