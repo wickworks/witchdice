0ba746fa6a3ae070717b26c9d716536ca8faa024
Olive Perry <wick@nitidbit.com>
2021-09-21 16:12:08 -0700

    Added link to google form.

--- src/components/settings/SiteAbout.jsx
+++ src/components/settings/SiteAbout.jsx
@@ -19,18 +19,22 @@ const SiteAbout = () => {
       <p>
         You can see current bugs and someday-features on
         this <a href="https://trello.com/b/e24TNiu1/witchdice" target="_blank" rel="noopener noreferrer">Trello board</a>.
-        The change log is at the bottom of this page.
+        Account-holders can vote on items.
       </p>
 
       <p>
-        If you find a bug or have an idea for a cool feature, please let me know in one of these places:
+        If you find a bug or have an idea for a cool feature, please let me know in one of these places!
         <ul>
-          <li>this google form</li>
+          <li>
+            this <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLScs5LFyqCURtVjQDzrscMZhWXC45xZl4sUdLLMig0QQ3fO5GA/viewform?usp=sf_link"
+              target="_blank" rel="noopener noreferrer">
+            google form</a>
+          </li>
           <li>on <a href="https://twitter.com/wickglyph" target="_blank" rel="noopener noreferrer">Twitter</a></li>
           <li>at contact@wick.works</li>
         </ul>
-
-
+        <em>(also, compliments are welcome. If you like this, be loud about it! I have social anxiety and no SEO savvy)</em>
       </p>
     </div>
   )
