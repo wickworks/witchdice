1bb66fd650c23b42e9acd3e599e96fc6056ade0b
Olive Perry <wick@nitidbit.com>
2021-11-22 07:44:27 -0800

    Autogun fixed damage works

--- src/components/lancer/data.js
+++ src/components/lancer/data.js
@@ -9,20 +9,26 @@ const getGrit = (pilot) => { return Math.ceil(pilot.level * .5) }
 
 // turns '10d6+4' into {count: 10, dietype: 6, bonus: 4}
 const processDiceString = (diceString) => {
-  var dice = diceString
+  var dice = String(diceString)
 
-  // slice off the bonus
-  var bonus = 0;
-  const bonusIndex = dice.indexOf('+');
-  if (bonusIndex >= 0) {
-    bonus = parseInt(dice.slice(bonusIndex+1))
-    dice = dice.slice(0, bonusIndex)
+  console.log('processing ', diceString);
+
+  if (dice && dice.indexOf('d') > 0) {
+    // slice off the bonus
+    var bonus = 0;
+    const bonusIndex = dice.indexOf('+');
+    if (bonusIndex >= 0) {
+      bonus = parseInt(dice.slice(bonusIndex+1))
+      dice = dice.slice(0, bonusIndex)
+    }
+
+    const count = parseInt(dice.slice(0, dice.indexOf('d')));
+    const dietype = parseInt(dice.slice(dice.indexOf('d')+1));
+
+    return {count: count, dietype: dietype, bonus: bonus}
   }
 
-  const count = parseInt(dice.slice(0, dice.indexOf('d')));
-  const dietype = parseInt(dice.slice(dice.indexOf('d')+1));
-
-  return {count: count, dietype: dietype, bonus: bonus}
+  return {count: 0, dietype: 'd0', bonus: diceString}
 }
 
 const findTagData = (tagID) => {
