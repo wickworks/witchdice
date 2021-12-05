4b87d4fd6a2941f3513b0fc042e2db06cb0af6e5
Olive Perry <wick@nitidbit.com>
2021-12-04 21:05:20 -0800

    Turns out it looks a lot better without any animation at all. Cest la vie. Added structure/stress

--- src/components/lancer/MechState/MechNumberBar.jsx
+++ src/components/lancer/MechState/MechNumberBar.jsx
@@ -9,48 +9,40 @@ const MechNumberBar = ({
   currentNumber,
   setCurrentNumber,
   leftToRight = true,
+  skipManualInput = false,
 }) => {
-  const [hoveringNumber, setHoveringNumber] = useState(null);
-
   // const displayNumber = hoveringNumber !== null ? hoveringNumber : currentNumber
 
   return (
-    <div className={`MechNumberBar ${extraClass}`}>
+    <div className={`MechNumberBar ${extraClass} ${leftToRight ? 'left-to-right' : 'right-to-left'}`}>
 
       <div className='label-container'>
-        {label}
-        <span className='numerical-count'>
-          {currentNumber}
-          /
-          {maxNumber}
-        </span>
+        {!skipManualInput &&
+          <div className='numerical-count'>
+            <input type='number' min={0} max={maxNumber} value={currentNumber} onChange={e => setCurrentNumber(e.target.value)} />
+            /
+            {maxNumber}
+          </div>
+        }
+        <div className='label'>
+          {skipManualInput && <span>{currentNumber}</span>}
+          {label}
+        </div>
       </div>
 
-      <div className={`ticks-container ${leftToRight ? 'left-to-right' : 'right-to-left'}`}>
+      <div className='ticks-container'>
 
         <button
-          className={`tick ${currentNumber > 0 ? 'filled' : 'empty'}`}
+          className={`tick zero ${currentNumber > 0 ? 'filled' : 'empty'}`}
           onClick={() => setCurrentNumber(0)}
-          onMouseEnter={() => setHoveringNumber(0)}
-          onMouseLeave={() => setHoveringNumber(null)}
         />
 
         { [...Array(maxNumber)].map((undef, i) => {
           const filledClass = (i < currentNumber) ? 'filled' : 'empty'
-
-          var nearClass = 'not-hovering'
-          if (hoveringNumber !== null) {
-            var distance = Math.abs(hoveringNumber - (i+1))
-            // if (hoveringNumber - i < 0) distance += 2
-            nearClass = distance < 6 ? `nearby-${distance}` : 'distant'
-          }
-
           return (
             <button
-              className={`tick ${filledClass} ${nearClass}`}
+              className={`tick ${filledClass}`}
               onClick={() => setCurrentNumber(i+1)}
-              onMouseEnter={() => setHoveringNumber(i+1)}
-              onMouseLeave={() => setHoveringNumber(null)}
               key={`${label}-tick-${i}`}
             >
               <div className='asset dot' />
--- src/components/lancer/MechState/MechState.jsx
+++ src/components/lancer/MechState/MechState.jsx
@@ -16,6 +16,9 @@ const MechState = ({
   const [currentHP, setCurrentHP] = useState(activeMech.current_hp);
   const [currentHeat, setCurrentHeat] = useState(activeMech.current_heat);
 
+  const [currentStructure, setCurrentStructure] = useState(4);
+  const [currentStress, setCurrentStress] = useState(4);
+
   console.log('activemech', activeMech);
 
   return (
@@ -27,23 +30,48 @@ const MechState = ({
 
       <div className='health-and-heat'>
 
-        <MechNumberBar
-          label="HP"
-          extraClass='hp-bar'
-          maxNumber={getMechMaxHP(activeMech, activePilot)}
-          currentNumber={currentHP}
-          setCurrentNumber={setCurrentHP}
-          leftToRight={false}
-        />
 
-        <MechNumberBar
-          label="Heat Cap"
-          extraClass='heat-bar'
-          maxNumber={getMechMaxHeatCap(activeMech, activePilot)}
-          currentNumber={currentHeat}
-          setCurrentNumber={setCurrentHeat}
-          leftToRight={true}
-        />
+        <div className='health-container'>
+          <MechNumberBar
+            label="Health"
+            extraClass='hp-bar'
+            maxNumber={getMechMaxHP(activeMech, activePilot)}
+            currentNumber={currentHP}
+            setCurrentNumber={setCurrentHP}
+            leftToRight={false}
+          />
+
+          <MechNumberBar
+            label="Structure"
+            extraClass='structure condensed'
+            maxNumber={4}
+            currentNumber={currentStructure}
+            setCurrentNumber={setCurrentStructure}
+            leftToRight={false}
+            skipManualInput={true}
+          />
+        </div>
+
+        <div className='heat-container'>
+          <MechNumberBar
+            label="Heat"
+            extraClass='heat-bar'
+            maxNumber={getMechMaxHeatCap(activeMech, activePilot)}
+            currentNumber={currentHeat}
+            setCurrentNumber={setCurrentHeat}
+            leftToRight={true}
+          />
+
+          <MechNumberBar
+            label="Stress"
+            extraClass='stress condensed'
+            maxNumber={4}
+            currentNumber={currentStress}
+            setCurrentNumber={setCurrentStress}
+            leftToRight={true}
+            skipManualInput={true}
+          />
+        </div>
       </div>
 
 
