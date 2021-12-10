a460195ba3c256ee0925ab76c6d375f9a82f5348
Olive Perry <wick@nitidbit.com>
2021-12-09 21:22:33 -0800

    calculate max mech HP

--- src/components/lancer/MechState/mechStateUtils.js
+++ src/components/lancer/MechState/mechStateUtils.js
@@ -4,13 +4,47 @@
 // } from '../../../utils.js';
 
 import {
-  OVERCHARGE_SEQUENCE
+  OVERCHARGE_SEQUENCE,
+  getGrit,
+  findSystemData,
+  findCoreBonusData,
 } from '../lancerData.js';
 
 
 export function getMechMaxHP(activeMech, activePilot, frameData) {
+  // start with frame base stat
+  var total = parseInt(frameData.stats.hp)
 
-  return parseInt(frameData.stats.hp);
+  // add pilot grit
+  total += getGrit(activePilot)
+
+  // add pilot skill
+  const hase = activePilot.mechSkills
+  const hull = hase[0]
+  total += hull * 2
+
+  // add stuff from systems i.e. personalizations
+  const loadout = activeMech.loadouts[0]
+  loadout.systems.forEach(system => {
+    const systemBonuses = findSystemData(system.id).bonuses;
+    if (systemBonuses) {
+      systemBonuses.forEach(bonus => {
+        if (bonus.id === 'hp') total += bonus.val
+      })
+    }
+  })
+
+  // add stuff from core bonuses i.e. that one IPSN one
+  activePilot.core_bonuses.forEach(coreBonusID => {
+    const coreBonusBonuses = findCoreBonusData(coreBonusID).bonuses;
+    if (coreBonusBonuses) {
+      coreBonusBonuses.forEach(bonusBonus => {
+        if (bonusBonus.id === 'hp') total += bonusBonus.val
+      })
+    }
+  })
+
+  return total;
 }
 
 export function getMechMaxHeatCap(activeMech, activePilot, frameData) {
