388669db80bbca4282dc79f49b8090f93e930951
Olive Perry <wickbox@gmail.com>
2022-02-12 12:04:36 -0800

    Ranged and melee synergies show up appropriately

--- src/components/lancer/WeaponRoller/synergyUtils.js
+++ src/components/lancer/WeaponRoller/synergyUtils.js
@@ -4,12 +4,26 @@ export function getFailingWeaponSynergies(weaponData, synergies) {
   const failingSynergies = synergies.filter(synergy => {
     // Weapon type? (mimic gun counts as everything)
     if (synergy.weapon_types && synergy.weapon_types[0] !== 'any') {
+
+      // // All ranged weapons
+      if (synergy.weapon_types.includes('Ranged')) {
+        if (weaponData.range.find(range => range.type === 'Range')) return false // this didn't fail
+      }
+      //
+      // // All melee weapons
+      if (synergy.weapon_types.includes('Melee')) {
+        if (weaponData.range.find(range => range.type === 'Threat')) return false // this didn't fail
+      }
+
+      // Rifle, CQB, etc
       if (!synergy.weapon_types.includes(weaponData.type) && weaponData.type !== '???') return true
     }
+
     // Weapon size?
     if (synergy.weapon_sizes && synergy.weapon_sizes[0] !== 'any') {
       if (!synergy.weapon_sizes.includes(weaponData.mount)) return true
     }
+
     return false
   })
 
