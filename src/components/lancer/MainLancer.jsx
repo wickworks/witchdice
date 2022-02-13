7c7a2fc0aa5fc5fbe5a416583771cdb3a4500c5d
Olive Perry <wickbox@gmail.com>
2022-02-13 13:32:20 -0800

    Broke all player & mech components into its own main file

--- src/components/lancer/MainLancer.jsx
+++ src/components/lancer/MainLancer.jsx
@@ -1,30 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { FileList, PlainList } from './FileAndPlainList.jsx';
-import EntryList from '../shared/EntryList.jsx';
-import JumplinkPanel from './JumplinkPanel.jsx';
-import FullRepairButton from './FullRepairButton.jsx';
-import PilotDossier from './PilotDossier.jsx';
-import MechSheet from './MechSheet/MechSheet.jsx';
-import SquadPanel from './SquadPanel/SquadPanel.jsx';
+import { FileList } from './FileAndPlainList.jsx';
+import MainLancerPlayers from './MainLancerPlayers.jsx';
 
 import PromisifyFileReader from 'promisify-file-reader'
 import { parseContentPack } from './contentPackParser.js';
-
-import compendiaJonesJson from './pilot_data/YOURGRACE.json';
 
 import {
   saveLcpData,
   loadLcpData,
   deleteLcpData,
-  savePilotData,
-  loadPilotData,
-  deletePilotData,
-  PILOT_PREFIX,
   LCP_PREFIX,
   STORAGE_ID_LENGTH,
 } from './lancerLocalStorage.js';
-
-import { findSystemData, findWeaponData } from './lancerData.js';
 
 import { deepCopy } from '../../utils.js';
 import { getIDFromStorageName } from '../../localstorage.js';
@@ -48,221 +35,7 @@ const MainLancer = ({
   const [activeLcpID, setActiveLcpID] = useState(coreLcpEntry.id);
   const [isShowingLcpList, setIsShowingLcpList] = useState(false);
 
-  const [allPilotEntries, setAllPilotEntries] = useState([]);
-  const [activePilotID, setActivePilotID] = useState(null);
-  const [activeMechID, setActiveMechID] = useState(null);
-
   const [triggerRerender, setTriggerRerender] = useState(false);
-
-  // const activePilot = allPilotEntries.find(pilot => pilot.id === activePilotID);
-  const activePilot = activePilotID && loadPilotData(activePilotID); // load the pilot data from local storage
-  const allMechEntries = activePilot ? activePilot.mechs : [];
-  const activeMech = allMechEntries.find(mech => mech.id === activeMechID);
-
-  // =============== INITIALIZE ==================
-  useEffect(() => {
-
-    // Save some dummy data (it's my OC, okay? I can have this)
-    savePilotData(compendiaJonesJson)
-
-    let pilotEntries = [];
-    let lcpEntries = [coreLcpEntry];
-
-    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
-      const key = localStorage.key(i);
-      // console.log('localStorage key : ', key);
-      // console.log('                item : ', localStorage.getItem(key));
-
-      if (key.startsWith(`${PILOT_PREFIX}-`)) {
-        const pilotID = getIDFromStorageName(PILOT_PREFIX, key, STORAGE_ID_LENGTH);
-        const pilotData = loadPilotData(pilotID);
-        if (pilotData) pilotEntries.push(pilotData);
-      }
-
-      if (key.startsWith(`${LCP_PREFIX}-`)) {
-        const lcpID = getIDFromStorageName(LCP_PREFIX, key, STORAGE_ID_LENGTH);
-        const lcpData = loadLcpData(lcpID);
-
-        let newEntry = {...coreLcpEntry}
-        newEntry.name = lcpData.manifest.name;
-        newEntry.id = lcpData.id;
-
-        lcpEntries.push(newEntry);
-      }
-    }
-
-    setAllPilotEntries(pilotEntries.map(pilot => ({name: pilot.name, id: pilot.id})));
-    setAllLcpEntries(lcpEntries);
-
-    // if we were looking at a pilot, restore tham and their first mech
-    const oldSelectedID = localStorage.getItem("lancer-selected-character");
-    if (oldSelectedID) {
-      const newActivePilot = pilotEntries.find(pilot => pilot.id.startsWith(oldSelectedID));
-      if (newActivePilot) {
-        setActivePilot(newActivePilot.id)
-      }
-    }
-  }, []);
-
-  // always select the last mech from a pilot
-  // useEffect(() => {
-  //   if (activePilot && activePilot.mechs.length > 0) {
-  //     const lastMechIndex = activePilot.mechs.length - 1
-  //     setActiveMechID(activePilot.mechs[lastMechIndex].id);
-  //   }
-  // }, [activePilotID]);
-
-
-  // =============== PILOT FILES ==================
-  const uploadPilotFile = e => {
-    const fileReader = new FileReader();
-    fileReader.readAsText(e.target.files[0], "UTF-8");
-    fileReader.onload = e => {
-      createNewPilot( JSON.parse(e.target.result) )
-    };
-  }
-
-  const createNewPilot = (pilot) => {
-    // sanity-check the pilot file
-    if (!pilot || !pilot.id || !pilot.mechs) return
-
-    let newData = [...allPilotEntries]
-
-    // remove any existing pilots of this ID
-    let pilotIndex = allPilotEntries.findIndex(entry => entry.id === pilot.id);
-    if (pilotIndex >= 0) newData.splice(pilotIndex, 1)
-
-    // store the entry & set it to active
-    newData.push({name: pilot.name, id: pilot.id});
-    setAllPilotEntries(newData);
-    setActivePilot(pilot.id)
-
-    // save to localstorage
-    savePilotData(pilot)
-  }
-
-  const setActivePilot = (pilotID) => {
-    const newActivePilot = pilotID && loadPilotData(pilotID)
-
-    if (newActivePilot) {
-      setActivePilotID(pilotID);
-
-      // select the last mech
-      if (newActivePilot && newActivePilot.mechs.length > 0) {
-        const lastMechIndex = newActivePilot.mechs.length - 1
-        setActiveMechID(newActivePilot.mechs[lastMechIndex].id);
-      } else {
-        setActiveMechID(null)
-      }
-
-      localStorage.setItem("lancer-selected-character", pilotID.slice(0,STORAGE_ID_LENGTH));
-    }
-  }
-
-  const deleteActivePilot = () => {
-    deletePilotData(activePilot)
-    localStorage.setItem("lancer-selected-character", '');
-
-    // remove from the current list of pilot entries
-    let pilotIndex = allPilotEntries.findIndex(entry => entry.id === activePilot.id);
-    if (pilotIndex >= 0) {
-      let newData = [...allPilotEntries]
-      newData.splice(pilotIndex, 1)
-      setAllPilotEntries(newData);
-    }
-
-    setActivePilotID(null);
-    setActiveMechID(null);
-  }
-
-  // =============== MECH STATE ==================
-
-  const updateMechState = (newMechData) => {
-    let newPilotData = deepCopy(activePilot);
-    const pilotIndex = allPilotEntries.findIndex(entry => entry.id === activePilot.id);
-    const mechIndex = activePilot.mechs.findIndex(mech => mech.id === activeMechID)
-    const loadout = newPilotData.mechs[mechIndex].loadouts[0]
-
-    if (mechIndex >= 0 && pilotIndex >= 0) {
-      Object.keys(newMechData).forEach(statKey => {
-        // update something on the pilot, actually
-        if (statKey === 'custom_counters' || statKey === 'counter_data') {
-          newPilotData[statKey] = newMechData[statKey]
-
-        // update the state of a system on the mech (modifying a value in a json tree is hell)
-        } else if (statKey === 'systemUses') {
-          const systemIndex = newMechData[statKey].index
-          loadout.systems[systemIndex].uses = newMechData[statKey].uses
-        } else if (statKey === 'systemDestroyed') {
-          const systemIndex = newMechData[statKey].index
-          loadout.systems[systemIndex].destroyed = newMechData[statKey].destroyed
-        } else if (statKey === 'weaponDestroyed') {
-          const mountSource = newMechData[statKey].mountSource
-          const mountIndex = newMechData[statKey].mountIndex
-          const weaponIndex = newMechData[statKey].weaponIndex
-          let slot
-          if (mountSource === 'mounts') {
-            slot = loadout.mounts[mountIndex].slots[weaponIndex]
-            if (!slot) slot = loadout.mounts[mountIndex].extra[0]
-          } else if (mountSource === 'improved_armament') {
-            slot = loadout.improved_armament.slots[weaponIndex]
-            if (!slot) slot = loadout.improved_armament.extra[0]
-          } else if (mountSource === 'integratedWeapon') {
-            slot = loadout.integratedWeapon.slots[weaponIndex]
-            if (!slot) slot = loadout.integratedWeapon.extra[0]
-          } else if (mountSource === 'integratedMounts') {
-            // integrated mounts can't be destroyed; only including here for thouroughness
-          }
-          slot.weapon.destroyed = newMechData[statKey].destroyed
-        } else if (statKey === 'repairAllWeaponsAndSystems') {
-          // - systems - //
-          [loadout.systems, loadout.integratedSystems].forEach(systemArray => {
-            systemArray.forEach(system => {
-              // Repair
-              system.destroyed = false
-              // Restore limited uses
-              const systemData = findSystemData(system.id)
-              if (systemData && systemData.tags) {
-                const limitedTag = systemData.tags.find(tag => tag.id === 'tg_limited')
-                if (limitedTag) system.uses = limitedTag.val
-              }
-            })
-          });
-          // - weapons - //
-          [loadout.mounts, [loadout.improved_armament], [loadout.integratedWeapon]].forEach(weaponMounts => {
-            weaponMounts.forEach(mount => {
-              mount.slots.forEach(slot => {
-                if (slot.weapon) {
-                  // Repair
-                  slot.weapon.destroyed = false
-                  // Restore limited uses
-                  const weaponData = findWeaponData(slot.weapon.id)
-                  if (weaponData && weaponData.tags) {
-                    const limitedTag = weaponData.tags.find(tag => tag.id === 'tg_limited')
-                    if (limitedTag) slot.weapon.uses = limitedTag.val
-                  }
-                }
-              })
-            })
-          });
-
-        // update a mech value
-        } else if (statKey === 'conditions') {
-          newPilotData.mechs[mechIndex][statKey] = newMechData[statKey]
-        } else {
-          newPilotData.mechs[mechIndex][statKey] = parseInt(newMechData[statKey])
-        }
-      });
-
-      // update it in localstorage
-      savePilotData(newPilotData)
-
-      setTriggerRerender(!triggerRerender)
-
-    } else {
-      console.error('Could not find mech or pilot to save it!', newMechData)
-    }
-  }
 
   // =============== LCP FILES ==================
   async function parseLcpFile(e) {
@@ -322,6 +95,26 @@ const MainLancer = ({
     setActiveLcpID(coreLcpEntry.id);
   }
 
+  // =============== INITIALIZE ==================
+  useEffect(() => {
+    let lcpEntries = [coreLcpEntry];
+
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+      const key = localStorage.key(i);
+      if (key.startsWith(`${LCP_PREFIX}-`)) {
+        const lcpID = getIDFromStorageName(LCP_PREFIX, key, STORAGE_ID_LENGTH);
+        const lcpData = loadLcpData(lcpID);
+
+        let newEntry = {...coreLcpEntry}
+        newEntry.name = lcpData.manifest.name;
+        newEntry.id = lcpData.id;
+
+        lcpEntries.push(newEntry);
+      }
+    }
+
+    setAllLcpEntries(lcpEntries);
+  }, []);
 
   return (
     <div className='MainLancer'>
@@ -382,66 +175,19 @@ const MainLancer = ({
         }
       </div>
 
-      { activePilot && <JumplinkPanel partyConnected={partyConnected} /> }
 
-      <FileList
-        title='Pilot'
-        extraClass='pilots'
-        allFileEntries={allPilotEntries}
-        setActiveFileID={setActivePilot}
-        activeFileID={activePilotID}
-        deleteActiveFile={deleteActivePilot}
-        onFileUpload={uploadPilotFile}
-        onFilePaste={parsedJson => createNewPilot(parsedJson)}
-      >
-        Upload a pilot data file (.json) from
-        <a href="https://compcon.app" target="_blank" rel="noopener noreferrer">COMP/CON</a>.
-      </FileList>
 
-      <div className='jumplink-anchor' id='pilot' />
-      { activePilot &&
-        <>
-          <PilotDossier
-            activePilot={activePilot}
-          />
+      <MainLancerPlayers
+        setTriggerRerender={setTriggerRerender}
+        triggerRerender={triggerRerender}
 
-          <PlainList title='Mech' extraClass='mechs'>
-            <EntryList
-              entries={allMechEntries}
-              handleEntryClick={setActiveMechID}
-              activeCharacterID={activeMechID}
-              deleteEnabled={false}
-            />
-          </PlainList>
-        </>
-      }
+        partyConnected={partyConnected}
+        partyRoom={partyRoom}
+        setPartyLastAttackKey={setPartyLastAttackKey}
+        setPartyLastAttackTimestamp={setPartyLastAttackTimestamp}
+        setRollSummaryData={setRollSummaryData}
+      />
 
-      <div className='jumplink-anchor' id='mech' />
-      { activeMech &&
-        <>
-          <FullRepairButton activeMech={activeMech} activePilot={activePilot} updateMechState={updateMechState} />
-          <MechSheet
-            activeMech={activeMech}
-            activePilot={activePilot}
-            updateMechState={updateMechState}
-
-            setPartyLastAttackKey={setPartyLastAttackKey}
-            setPartyLastAttackTimestamp={setPartyLastAttackTimestamp}
-            setRollSummaryData={setRollSummaryData}
-          />
-        </>
-      }
-
-      <div className='jumplink-anchor' id='squad' />
-      { partyConnected &&
-        <SquadPanel
-          activeMech={activeMech}
-          activePilot={activePilot}
-
-          partyConnected={partyConnected}
-          partyRoom={partyRoom}
-        />
-      }
 
       <div className='jumplink-anchor' id='dicebag' />
     </div>
