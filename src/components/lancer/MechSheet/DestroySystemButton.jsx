1a39fa792f6d8da28f72d8a343af5421deea9b69
Olive Perry <wickbox@gmail.com>
2022-02-26 11:12:21 -0800

    css animation for destroy/broadcast buttons

--- src/components/lancer/MechSheet/DestroySystemButton.jsx
+++ src/components/lancer/MechSheet/DestroySystemButton.jsx
@@ -9,10 +9,10 @@ const DestroySystemButton = ({
 
   return (
     <button className='DestroySystemButton' onClick={onDestroy}>
+      <div className={`asset ${isDestroyed ? 'repair' : 'x'}`} />
       <div className='hover-text'>
         <strong>{isDestroyed ? 'REPAIR' : 'DESTROY'}</strong>
       </div>
-      <div className={`asset ${isDestroyed ? 'repair' : 'x'}`} />
     </button>
   )
 }
@@ -28,10 +28,10 @@ const BroadcastSystemButton = ({
       onClick={() => { setHasBeenBroadcast(true); onBroadcast(); }}
       disabled={hasBeenBroadcast}
     >
+      <div className='asset sensor' />
       <div className='hover-text'>
         <strong>BROADCAST</strong>
       </div>
-      <div className='asset sensor' />
     </button>
   )
 }
