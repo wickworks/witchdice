47b476c29acad703e8c970313b9050e213c83f61
Olive Perry <wickbox@gmail.com>
2022-04-24 23:11:26 -0700

    Not all npc actions are invades; correctly classify them as their data specifies. (this prevents 2 heat from being added)

--- src/components/lancer/LancerNpcMode/NpcMechSheet.jsx
+++ src/components/lancer/LancerNpcMode/NpcMechSheet.jsx
@@ -265,11 +265,12 @@ function getNpcTechAttacks(items) {
   items.forEach((item, itemIndex) => {
     const featureData = findNpcFeatureData(item.itemID)
 
+
     if (featureData.type === 'Tech' && isNpcFeatureTechAttack(featureData)) {
       // const effectWithoutFirstSentence = featureData.effect.slice(featureData.effect.indexOf('.') + 1)
       techAttacks.push({
         "name": featureData.name,
-        "activation": "Invade",
+        "activation": featureData.tech_type ? `${featureData.tech_type} Tech` : "Quick Tech",
         "detail": featureData.effect,
       })
     }
