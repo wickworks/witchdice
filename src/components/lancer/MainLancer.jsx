05e06d5bbec5788de51ff433cb6a219054cb307b
Olive Perry <wickbox@gmail.com>
2022-06-29 08:59:33 -0600

    Update intro text.

--- src/components/lancer/MainLancer.jsx
+++ src/components/lancer/MainLancer.jsx
@@ -143,31 +143,30 @@ const MainLancer = ({
 
       <div className='wip-container'>
         <p>
-          Hi! Thanks for checking out the Witchdice Lancer tool.
-          It is a WIP, so expect bugs and missing features.
-          You can send me feedback & bugs via
+          Lancer is a big game; tell me about bugs or missing mechanics
           <a
             href='https://docs.google.com/forms/d/e/1FAIpQLScs5LFyqCURtVjQDzrscMZhWXC45xZl4sUdLLMig0QQ3fO5GA/viewform'
             target="_blank"
             rel="noopener noreferrer"
           >
-            google form
+            here
           </a>
-          or
+          or on
           <a
             href='https://twitter.com/wickglyph'
             target="_blank"
             rel="noopener noreferrer"
           >
-            Twitter
-          </a>,
-          and see the current to-do list on
+            Twitter.
+          </a>
+        </p>
+        <p>
           <a
             href='https://trello.com/b/e24TNiu1/witchdice'
             target="_blank"
             rel="noopener noreferrer"
           >
-            Trello.
+            Current to-do list.
           </a>
         </p>
       </div>
