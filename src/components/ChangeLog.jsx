d82dacc9f2307fea5bd8a8e3b2eab1b78dcdb94f
Olive Perry <wickbox@gmail.com>
2022-09-22 21:33:55 -0700

    v0.13.7

--- src/components/ChangeLog.jsx
+++ src/components/ChangeLog.jsx
@@ -14,8 +14,9 @@ const ChangeLog = () => {
 
       {isOpen &&
         <div className="change-container">
-          <div className='version-label'>v0.13.0-6 — July, Aug 2022</div>
+          <div className='version-label'>v0.13.0-7 — July, Aug, Sept 2022</div>
           <ul>
+            <li>Handle faulty LCP weapon profile data without crashing.</li>
             <li>Added warning for GM mode on missing NPC LCP file.</li>
             <li>Fix limited charges on mechs with multiple integrated mounts.</li>
             <li>Fix skill difficulty being incorrectly applied on some NPCs.</li>
