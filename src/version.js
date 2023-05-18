338b7562ec9cd3eee11c3c873a6f749240aafb98
Olive Perry <wickbox@gmail.com>
2023-05-18 15:47:59 -0700

    v1.1.10

--- src/version.js
+++ src/version.js
@@ -1,7 +1,7 @@
 
 // minor version increase clear out monsters, major ones clear characters
 // ^^^ as of 1.0.0, NO LONGER ^^^
-const CURRENT_VERSION = '1.1.9';
+const CURRENT_VERSION = '1.1.10';
 
 export {
   CURRENT_VERSION
