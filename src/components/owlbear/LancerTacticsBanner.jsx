e6a6eab99db6e25524c0e39c1212b41279e54e12
olive <olive@wick.works>
2025-05-12 09:16:45 -0700

    Hide the lancer tactics banner if you click it

--- src/components/owlbear/LancerTacticsBanner.jsx
+++ src/components/owlbear/LancerTacticsBanner.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './LancerTacticsBanner.scss';
 
-const LancerTacticsBanner = ({
+const CLICKED_BANNER_KEY = 'hide-lancer-tactics-banner'
 
-}) => {
+const LancerTacticsBanner = ({}) => {
+	const [bannerHidden, setBannerHidden] = useState(String(localStorage.getItem(CLICKED_BANNER_KEY)) === 'true')
 
-  return (
-    <div className='LancerTacticsBanner'>
-		<a href='https://wick.itch.io/lancer-tactics' referrerPolicy='origin' target='_blank'>
-			<div className='asset lancer_tactics_banner' />
-			{/*<p>We're making a Lancer video game!</p>*/}
-			{/*<div className='asset gen_nelson' />*/}
-		</a>
-		<p>( Like Witchdice? Help support my work by buying my Lancer video game! )</p>
+	const onHideBanner = () => {
+		localStorage.setItem(CLICKED_BANNER_KEY, 'true')
+		setBannerHidden(true)
+	}
+
+	return (
+		<div className='LancerTacticsBanner'>
+			{ (!bannerHidden) && <>
+				<a href='https://wick.itch.io/lancer-tactics' referrerPolicy='origin' target='_blank' onClick={onHideBanner}>
+					<div className='asset lancer_tactics_banner' />
+				</a>
+				<p>( Like Witchdice? Help support my work by buying my Lancer video game! )</p>
+			</>}
 		</div>
-  )
+	)
 }
 
 export default LancerTacticsBanner ;
