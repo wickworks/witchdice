519292caa160f46948f8f0b7f61a525d1e052e6c
olive <olive@wick.works>
2026-09-09 11:10:43 -0700

    update wip notice to link to the github

--- src/components/lancer/WipNotice.jsx
+++ src/components/lancer/WipNotice.jsx
@@ -5,23 +5,17 @@ import './WipNotice.scss';
 const WipNotice = () => (
   <div className='WipNotice'>
     <p>
-      Lancer is a big game; tell me about bugs or missing mechanics
-      <a
-        href='https://docs.google.com/forms/d/e/1FAIpQLScs5LFyqCURtVjQDzrscMZhWXC45xZl4sUdLLMig0QQ3fO5GA/viewform'
-        target="_blank"
-        rel="noopener noreferrer"
-      >here</a>
-      or on<a
-        href='https://bsky.app/profile/wick.works'
-        target="_blank"
-        rel="noopener noreferrer"
-      >Bsky.</a>
-
-      The to-do list is on<a
+      Witchdice is in support mode; I'll only be fixing critical errors.
+      If you find bugs or want to help with the<a
         href='https://trello.com/b/e24TNiu1/witchdice'
         target="_blank"
         rel="noopener noreferrer"
-      >Trello.</a>
+      >backlog,</a>
+      you can submit PRs on<a
+        href='https://github.com/wickworks/witchdice'
+        target="_blank"
+        rel="noopener noreferrer"
+      >Github.</a>
     </p>
   </div>
 )
